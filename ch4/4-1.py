0e7c3a349abc5c551bde754c06414ab5ced13364
nafan <topkekoutof10@gmail.com>
2021-04-05 15:46:04 -0500

    changes ch4

--- ch4/4-1.py
+++ ch4/4-1.py
@@ -1,5 +1,7 @@
 import turtle
-
+'''Write a void (non-fruitful) function to draw a square. Use it in a program to draw the image shown below. Assume 
+each side is 20 units. (Hint: notice that the turtle has already moved away from the ending point of the last square 
+when the program ends.) '''
 
 def make_window(color, title):
     w = turtle.Screen()
