7b601a2dd0c3f2056f8e82520005638c642c42ab
nafan <topkekoutof10@gmail.com>
2021-04-05 20:03:18 -0500

    changes ch6

--- ch6/6-1.py
+++ ch6/6-1.py
@@ -8,9 +8,22 @@ def test(did_pass):
         msg = ("Test at line {0} FAILED.".format(linenum))
     print(msg)
 
+def turn_clockwise(dir):
+    if dir == "N":
+        return "E"
+    elif dir == "E":
+        return "S"
+    elif dir == "S":
+        return "W"
+    elif dir == "W":
+        return "N"
+    return None
 
 def test_suite():
-    pass
+    test(turn_clockwise("N") == "E")
+    test(turn_clockwise("W") == "N")
+    test(turn_clockwise(42) == None)
+    test(turn_clockwise("rubbish") == None)
 
 test_suite()
 
